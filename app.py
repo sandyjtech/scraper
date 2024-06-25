33df040bada1649d2397225b0468c9e9ce483a47
sandyjtech <sandyjtech@gmail.com>
2024-06-25 12:31:59 -0700

    added qa

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 # app.py
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 from flask_cors import CORS
-
-
+from listen import AnalyzeCall
 from scraper import Scraper
 
 
@@ -11,8 +10,13 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
 
-
+api.add_resource(AnalyzeCall, '/analyze-call')
 api.add_resource(Scraper, "/scaper")
+
 if __name__ == "__main__":
     app.run(debug=True)
